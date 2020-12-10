10f421daa46cd23bdb57a377225ca386e6dce61b
Kawasaki Shohei <kawasaki.shohei.1@gmail.com>
2020-12-10 09:13:33 +0000

    [Add] ログイン機能を追加

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'font-awesome-sass', '~> 5.13'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-byebug'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#top'
+  get 'home/about', to: 'home#about'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#top'
-  get 'home/about', to: 'home#about'
   resources :users, only: [:show]
 end
