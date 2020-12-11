c567025f05b0bc991338c8174ed0eccc3677053e
Kawasaki Shohei <kawasaki.shohei.1@gmail.com>
2020-12-11 11:42:07 +0000

    [Update] ログインしている時のヘッダーの遷移先を修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def top
+    if user_signed_in?
+      redirect_to user_path(current_user)
+    end
   end
 
   def about
