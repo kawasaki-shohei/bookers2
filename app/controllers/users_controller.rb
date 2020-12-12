a1300dcc0543ce470c768a5601dc01f1c6e36b58
Kawasaki Shohei <kawasaki.shohei.1@gmail.com>
2020-12-12 12:40:58 +0000

    [Update] 他人のユーザー情報編集画面へ遷移したときのリダイレクト先を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if @user != current_user
-      redirect_to edit_user_path(current_user)
+    unless @user == current_user
+      redirect_to user_path(current_user)
     end
   end
 
